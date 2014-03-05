aea2da02c99112a56be2f276b83d615b6eb5dcf1
liamjbennett <lbennett@opentable.com>
2014-03-05 15:34:11 +0000

    simplify travis.yml

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-bundle exec rake syntax
-bundle exec rake lint
-bundle exec rake spec
-bundle exec rspec spec/acceptance
+bundle exec rake test
+
+RS_SET=ubuntu-server-12042-x64 RS_DEBUG=yes bundle exec rspec spec/acceptance
+RS_SET=centos-64-x64 RS_DEBUG=yes bundle exec rspec spec/acceptance
